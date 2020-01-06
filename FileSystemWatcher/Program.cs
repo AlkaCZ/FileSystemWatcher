cece95241916fa42b3883969e13445d092630456
Meterologická stanice <petrjelinek35@seznam.cz>
2020-01-06 15:27:07 +0100

    Přidejte soubory projektu.

---
+++ FileSystemWatcher/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Security.Permissions;
+
+//Příklad:
+//Vytvořte program, který přijme adresář a na konzoli vypisuje všechny nově vytvořené soubory, smazané soubory a přejmenované soubory.K realizaci použijte třídu FileSystemWatcher.
+
+namespace Watcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemWatcher FSW = new FileSystemWatcher();
+            FSW.Created += FSW_Vytvoreno;
+            FSW.Deleted += FSW_Smazano;
+            FSW.Changed += FSW_Zmeneno;
+            FSW.Renamed += FSW_Prejmenovano;
+            // Path
+            FSW.Path = $@"D:\PVA\PVA - 3. Ročník\FileSystemWatcher\FileSystemWatcher";
+            //
+            FSW.EnableRaisingEvents = true;
+            Console.ReadLine();
+        }
+
+        private static void FSW_Prejmenovano(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"Soubor {e.OldName} byl přejmenován na {e.Name}!");
+        }
+
+        private static void FSW_Zmeneno(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Soubor {e.Name} byl upraven!");
+        }
+
+        private static void FSW_Smazano(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Soubor {e.Name} byl smazán!");
+        }
+
+        private static void FSW_Vytvoreno(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Byl vytvořen nový soubor {e.Name}!");
+        }
+    }
+
+}
